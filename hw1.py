35397638087ef6c9f7f3bd1cbbba276098ad2608
Brittany Lee <Brittany@dhcp-10-101-140-180.wireless.northwestern.private>
2014-09-29 23:00:56 -0500

    update equations

--- hw1.py
+++ hw1.py
@@ -6,7 +6,7 @@ def c1(a, pm, pc, rc):
 	radius = rc / a;
 
 	# moment of inertia
-	I = (8 / 15) * math.pi * ((pm * math.pow(a,5)) + ((pc - pm) * math.pow(rc,3)))
+	I = (8 / 15) * math.pi * ((pm * math.pow(a,5)) + ((pc - pm) * math.pow(rc,5)))
 
 	# mass
 	M = (4 / 3) * math.pi * ((pm * math.pow(a,3)) + ((pc - pm) * math.pow(rc,3)))
@@ -26,17 +26,17 @@ print("b. pm = pc")
 print(b)
 print ("When pm = pc, I/Ma^2 equals 0.4 because there is uniform density distribution\n")
 
-c = c1(7439, 2555, 0, 0.99*7439)
+c = c1(5, 1, 0, 0.99*5)
 print ("c. pc = 0 and rc = 0.99a")
 print(c)
 print ("When pc = 0 and rc = 0.99a, I/Ma^2 equals 0.667 because all of the density is in the shell\n")
 
-d = c1(2439, 2555, 5427, 1220)
+d = c1(2439, 2100, 5427, 1220)
 print ("d. plausible two-layer model for Mercury")
 print (d)
 print("A plausible two-layer model for Mercury assuming that I/Ma^2 equals 0.35 is")
-print("a = 2439")
-print ("pm = 2555")
-print ("pc = 5427")
-print ("rc = 1220")
+print("a = 2439 km")
+print ("pm = 2100 kg/m^3")
+print ("pc = 5427 kg/m^3")
+print ("rc = 1220 km")
 
